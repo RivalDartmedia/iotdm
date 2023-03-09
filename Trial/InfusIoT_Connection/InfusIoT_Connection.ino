e4c48518bf22e670a80b2c15c1c91e135df3bd4a
Andy lucky <45935449+Lucranix28@users.noreply.github.com>
2023-03-09 14:29:31 +0700

    -

---
+++ Trial/InfusIoT_Connection/InfusIoT_Connection.ino
@@ -0,0 +1,19 @@
+#include "memory.h"
+#include "sensor.h"
+#include "koneksi.h"
+#include "indikator.h"
+
+void setup(){
+    //STEP1: Init Memory
+    //STEP2: Load Config
+    //STEP3: Config if needed
+    //STEP4: Init Connection
+    //STEP5: Init Sensor
+}
+
+void loop() {
+    //STEP-L1: Connection Management
+    //STEP-L2: Check Command
+    //STEP-L3: Get Sensor Data
+    //STEP-L4: Send Data
+}
---
+++ Trial/InfusIoT_Connection/memory.h
@@ -0,0 +1,16 @@
+#ifndef memory_h
+#define memory_h
+
+#include <ArduinoJson.h>
+#include <LittleFS.h>
+#ifdef ESP32
+#include "FS.h"
+#endif
+
+#define FORMAT_LITTLEFS_IF_FAILED true
+
+//Load dari Memori
+//Template Identifier
+
+
+#endif // !1
