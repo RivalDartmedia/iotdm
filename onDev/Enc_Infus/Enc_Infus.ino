f91e94ce7c020a720192f3a59d2ef9ee9c46cf29
Rivaldy <rivaldydartmedia@gmail.com>
2023-05-24 15:59:28 +0700

    pengembalian delay yg sempat tercoment

--- onDev/Enc_Infus/Enc_Infus.ino
+++ onDev/Enc_Infus/Enc_Infus.ino
@@ -141,7 +141,7 @@ void loop() {
     // displed.print_sample(val_sample_berat);
     displed.sample(val_sample_tpm, val_sample_berat);
 
-    // delay(2000);
+    delay(2000);
 
     connect1.connectWifi(config1);
 
