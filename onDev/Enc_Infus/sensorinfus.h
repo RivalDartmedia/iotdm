19075156f0b334057ddae4d7992979104054dce6
Rivaldy <rivaldydartmedia@gmail.com>
2023-07-20 13:45:58 +0700

    update filter so far

--- onDev/Enc_Infus/sensorinfus.h
+++ onDev/Enc_Infus/sensorinfus.h
@@ -15,7 +15,7 @@ private:
     byte lastReading;
     unsigned long lastDebounceTime;
     unsigned long debounceDelay = 20;
-    unsigned long notupdatelim = 60000;
+    unsigned long notupdatelim = 120000;
     int tpm_val_previous = 0;
 
 public:
@@ -43,11 +43,32 @@ public:
             // Update tpm
             tpm_val = (int) (60000 / (millis() - lastDebounceTime));
             // tpm_val = (float)(60000 / (millis() - lastDebounceTime));
-            if (tpm_val > 1000 || tpm_val == (tpm_val_previous)/2 || tpm_val == ((tpm_val_previous)/2)+1 || tpm_val == ((tpm_val_previous)/2)-1){
+            if (tpm_val > 1000 || tpm_val == (tpm_val_previous)/2 || tpm_val == ((tpm_val_previous)/2)+1 || tpm_val == ((tpm_val_previous)/2)-1 || tpm_val == ((tpm_val_previous)/2)-2 || tpm_val == ((tpm_val_previous)/2)-3 || tpm_val == ((tpm_val_previous)/2)-4 || tpm_val == ((tpm_val_previous)/2)-5){
                 tpm_val = tpm_val_previous;
-            } else {
-                tpm_val_previous = tpm_val;
+                Serial.println("SAMA SEPERTI SEBELUMNYA");
+            // } else {
+            //     tpm_val_previous = tpm_val;
+            // }
             }
+            tpm_val_previous = tpm_val;
+            // int threshold = 10;
+            // int dif = abs(tpm_val - tpm_val_previous);
+            // int cnt_tpm_val_lim = 5;
+            // int cnt_tpm_val;
+            // if (dif > threshold){
+            //     Serial.println("Selisih besar");
+            //     cnt_tpm_val++;
+            //     Serial.println(cnt_tpm_val);
+                // if (cnt_tpm_val > cnt_tpm_val_lim){
+                //     tpm_val = tpm_val;
+                //     cnt_tpm_val = 0;
+                //     Serial.println("Masuk IF");
+                // }else {
+                    // tpm_val = tpm_val_previous;
+                    // Serial.println("Masuk ELSE");
+                // }
+            // }
+            // tpm_val_previous = tpm_val;
         }
         lastReading = newReading;
     }
@@ -58,6 +79,11 @@ public:
         {
             // tpm tidak terupdate setelah notupdatelim ms, return 0
             tpm_val = 0;
+        }
+        if ((0 < tpm_val) && (tpm_val < 1))
+        {
+            tpm_val = 1;
+            Serial.println("NILAI DIANTARA 0 DAN 1");
         }
         return tpm_val;
     }
@@ -234,7 +260,7 @@ class Bat
 
     bool cek()
     {
-        float bat_volt = analogRead(bat_pin);
+        int bat_volt = analogRead(bat_pin);
         Serial.print("ADC : ");
         Serial.println(bat_volt);
         if (bat_volt == 0){
