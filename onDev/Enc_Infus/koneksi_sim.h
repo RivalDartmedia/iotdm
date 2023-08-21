39426d639c38a1772eec057b83aded3b5497a5e8
Rivaldy <rivaldydartmedia@gmail.com>
2023-08-21 16:52:04 +0700

    ganti sim ke url server

--- onDev/Enc_Infus/koneksi_sim.h
+++ onDev/Enc_Infus/koneksi_sim.h
@@ -133,9 +133,9 @@ public :
     // BLYNK :
     // String address = server_dom + send_p + token + tokenid + berat_v + String(weigh) + tpm_v + String(tpm);
     // Callmebot :
-    String address = server_dom_callmebot + send_p_callmebot + get_p_callmebot + token_callmebot + tokenid + "&text=" + "ID+Device+=+" + String(infusid) + ";+TPM+=+" + String(tpm) + "+;+Weigh+=+" + String(weigh);
+    // String address = server_dom_callmebot + send_p_callmebot + get_p_callmebot + token_callmebot + tokenid + "&text=" + "ID+Device+=+" + String(infusid) + ";+TPM+=+" + String(tpm) + "+;+Weigh+=+" + String(weigh);
     // API :
-    // String address = URL + prefixRoute + path + "?token=" + token_api + "&deviceId=" + String(infusid) + "&tpm=" + String(tpm) + "&weight=" + String(weigh);
+    String address = URL + prefixRoute + path + "?token=" + token_api + "&deviceId=" + String(infusid) + "&tpm=" + String(tpm) + "&weight=" + String(weigh);
     Serial.println(address);
     char URL1[304];
 
